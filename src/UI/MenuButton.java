8d3ba3a6e452f3c4340847d534f3d8a075d2fc77
Qwirvalt <legendofkoala@gmail.com>
2019-05-17 15:34:30 +0200

    Clics et créations de liens basiques

---
+++ src/UI/MenuButton.java
@@ -0,0 +1,23 @@
+package UI;
+
+public class MenuButton {
+
+    int posx;
+    int posy;
+    String name;
+
+
+    public MenuButton(int x, int y) {
+        this.posx = x;
+        this.posy = y;
+    }
+
+    public int getPosx() {
+        return this.posx;
+    }
+
+    public int getPosy() {
+        return this.posy;
+    }
+
+}
