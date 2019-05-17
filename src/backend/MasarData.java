19b8f4ae4a07d3311e154946c4cfc7504bfe3f56
Qwirvalt <legendofkoala@gmail.com>
2019-05-17 15:47:31 +0200

    modif seb

--- src/backend/MasarData.java
+++ src/backend/MasarData.java
@@ -2,6 +2,7 @@ package backend;
 
 import Objects.MasarSystem;
 import Objects.SystemLink;
+import UI.MenuButton;
 import backend.ClickManager;
 
 import java.util.LinkedList;
@@ -9,11 +10,13 @@ import java.util.LinkedList;
 public class MasarData{
     private LinkedList<MasarSystem> systemList;
     private LinkedList<SystemLink> linkList;
+    private LinkedList<MenuButton> buttonList;
 
     private ClickManager clickManager;
 
     public LinkedList<MasarSystem> getSystemList(){return this.systemList;}
     public LinkedList<SystemLink> getLinkList(){return this.linkList;}
+    public LinkedList<MenuButton> getButtonList() {return this.buttonList;}
 
     public ClickManager getClickManager(){return this.clickManager;}
     public void setClickManager(ClickManager c){this.clickManager = c;}
@@ -21,5 +24,6 @@ public class MasarData{
     public MasarData(){
         this.systemList = new LinkedList<MasarSystem>();
         this.linkList = new LinkedList<SystemLink>();
+        this.buttonList = new LinkedList<MenuButton>();
     }
 }
