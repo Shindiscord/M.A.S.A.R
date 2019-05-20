8ae5fd08bc25a5a88839b8f61ce203d568a539cf
Cepsy <maximegraziano@live.fr>
2019-05-20 16:17:36 +0200

    Calculs RPS Growth POP Modif Solar system

--- src/Masar.java
+++ src/Masar.java
@@ -46,15 +46,16 @@ class MasarGame implements Game {
            System.out.println("x : " + gameData.getCoordinates()[i][0] + " et y : " + gameData.getCoordinates()[i][1] );
         */
 
-        this.gameData.getSystemList().add(new MasarSystem(1,0,0,0,1, this.gameData));
-        this.gameData.getSystemList().add(new MasarSystem(0,0,0,0,3, this.gameData));
-        this.gameData.getSystemList().add(new MasarSystem(0,0,0,0,1, this.gameData));
-        this.gameData.getSystemList().add(new MasarSystem(2,0,0,0,2, this.gameData));
+        this.gameData.getSystemList().add(new MasarSystem(1,1,1, this.gameData));
+        this.gameData.getSystemList().add(new MasarSystem(0,0,3, this.gameData));
+        this.gameData.getSystemList().add(new MasarSystem(0,0,1, this.gameData));
+        this.gameData.getSystemList().add(new MasarSystem(0,0,2, this.gameData));
 
-        for (int i=4 ; i<24 ; i++)
-            this.gameData.getSystemList().add(new MasarSystem(0,0,0,0,1, this.gameData));
+        /*for (int i=4 ; i<24 ; i++)
+            this.gameData.getSystemList().add(new MasarSystem(0,0,1, this.gameData));
+        */
 
-        for (int i=0 ; i<24 ; i++)
+        for (int i=0 ; i<4 ; i++)
             this.gameData.getSystemList().get(i).setPos(gameData.getCoordinates()[i][0], gameData.getCoordinates()[i][1]);
 
 
